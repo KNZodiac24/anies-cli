ad12698993a3b06802d5c166095370764e2284f0
KNZodiac24 <navakenny_16@hotmail.com>
2024-09-20 19:00:00 -0500

    Se mejora patrón de búsqueda para comillas dobles

--- scraper.py
+++ scraper.py
@@ -7,14 +7,13 @@ sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 SITIO_ANIME = "https://animefenix.tv/"
 
-anime = "one piece"
+anime = "saikyou no"
 
 anime_busqueda = anime.replace(" ", "+")
 
 sitio_busqueda = requests.get(SITIO_ANIME+"animes?q="+anime_busqueda).text
 
-patron = r"title=\".*\""
-
+patron = r"title=\".*\"\>"
 lista_resultado_busqueda = re.findall(patron, str(sitio_busqueda))
 lista_sin_repetidos = list(set(lista_resultado_busqueda))
 lista_sin_repetidos.sort()
@@ -24,6 +23,9 @@ if lista_sin_repetidos.__len__() > 0:
     for resultado in lista_sin_repetidos:
         resultado = resultado.replace('title="', "")
         resultado = resultado.replace('"', "")
+        if "&quot;" in resultado:
+            resultado = resultado.replace("&quot;", '"')
+        resultado = resultado.replace(">", "")
         print(resultado)
 
 else:
